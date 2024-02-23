a2fad24951928291b3778c0a95a5037df4f9aa18
skociuba <skociuba83@gmail.com>
2024-02-23 19:28:24 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 export default async function Home() {
   return (
-    <div className="bg-custom bg-size-custom flex min-h-screen w-full flex-col items-center justify-center bg-fixed text-white">
+    <div>
+      {' '}
+      {/* prettier-ignore */}
+      <div className="bg-custom bg-size-custom flex min-h-screen w-full flex-col items-center justify-center bg-fixed text-white">
       <div className="fixed inset-0 bg-black/70" />
       <div className="z-10">CV Creator</div>
+    </div>
     </div>
   );
 }
