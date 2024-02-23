a79ef387da64cd7b2904d97f6afe3a8b409214f3
skociuba <skociuba83@gmail.com>
2024-02-23 13:00:27 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
 
   return (
     <section className="flex flex-col items-center justify-center">
-      <h1>Home</h1>
+      <h1>Homes</h1>
       <h1>Server Side Rendered</h1>
       <pre>{JSON.stringify(session)}</pre>
       <h1>Client Side Rendered</h1>
