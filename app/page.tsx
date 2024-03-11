e2dbca05343ed7ea726e79a0efb4ef585aa8c7d0
skociuba <skociuba83@gmail.com>
2024-03-11 13:57:43 +0100

    fix text

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
       <div className="z-10 mt-10 font-bold text-left mx-[400px] ">
       <div className="pb-5 z-10 text-6xl">Get a job!</div>
       <div className="pl-2 pb-10 text-3xl">CV Creator</div>
-      <div className="pl-2 text-xl flex flex-col flex-wrap text-justify opacity-60">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>
+      <div className="pl-2 text-xl flex flex-col flex-wrap text-justify opacity-70">The curriculum vitae, or CV, is an essential component of the job search process, aiding employers in getting a better understanding of candidates. Information such as professional experience, education, and skills contained within it allows for a quick grasp of the potential benefits a candidate can bring to an organization. With a carefully crafted CV, candidates have the opportunity to showcase their professional accomplishments and stand out among others. It serves not only as a document but also as a tool for effective job hunting and building a professional reputation. In today&apos;s competitive job market, a well-prepared CV can be the key to professional success.</div>
       </div>
     </div>
     </div>
